03754f665ebb16ff3f9be996dbcd514fbb72baa8
Itz-Djin <Christdjin.B@gmail.com>
2022-06-14 15:04:15 -0600

    Completed Course Assignments

---
+++ JavaScript/Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,33 @@
+window.alert("Practicing alerts with JS in HTML")
+document.write("Hello World! were using a document.type method in a JS file to write this line")
+
+var X = " |This is a string value!"
+document.write(X)
+
+// STRING ASSIGNMENT
+var A = "Creating a string value for Variable A"
+window.alert(A)
+
+// ESCAPE CHARACTER ASSIGNMENT
+var B = " At yesterdays Barbeque, my niece said, \'How much wood could a woodchuck, chuck if a woodchuck could chuck wood?\' "
+document.write(B)
+
+// CONCATENATION ASSIGNMENT
+var C = " \"Ever since I was a child I have had this instinctive urge for expansion and growth."
+    + " To me, the function and duty of a quality human being is the sincere"
+    + " and honest development of one\'s potential \" "
+document.write(C)
+
+// MULTIPLE VARIABLES ASSIGNMENT
+var D = "Cars "
+Toyota = "Tacoma "
+Mazda = "RX7 "
+Honda = "Accord "
+Jeep = "Wrangler "
+document.write(Toyota)
+
+// EXPRESSION ASSIGNMENT
+var E = 3 
+F = 7
+var G = (E + F) // This is the expression
+document.write(G)
