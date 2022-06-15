a2a8034d39d50597b71efbd2526a749a88991690
Itz-Djin <Christdjin.B@gmail.com>
2022-06-14 20:23:47 -0600

    Project Functions

---
+++ JavaScript/Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function changeColor(newColor) {  //keyword pluse name created 'changeColor' paramaters ask for 'newColor'
+    const elem = document.getElementById('para'); // keyword const, references value that invokes the getElementByID method that selects the para Id on the HTML Doc
+    elem.style.color = newColor; 
+}
+
+function myFunction() { // created variable myFunction
+    var sentence = "I am learning"; // created variable sentence with a string value
+    sentence += " a lot from this book!"; // added += operator to attach another sting value to the sentence variable
+    document.getElementById("conc").innerHTML = sentence; // invoked getElementById on html document selecting the element with id 'conc' to effect whats on the inside of the element using the '.innerHTML' property
+}
