e13bd91815c62a31bb24473d749460e5bc011e28
Itz-Djin <Christdjin.B@gmail.com>
2022-06-16 20:37:49 -0600

    Created more functions utilizing different operators
    Made use of subtraction, multiplication, division and a final equation using all combined.

--- JavaScript/Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ JavaScript/Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,23 @@ function add() {
     var addition = 5 + 5;
     document.getElementById('Math').innerHTML = '5 + 5 =  ' + addition;
 }
+
+function sub() {
+    var subtraction = 50 - 25;
+    document.getElementById('Math2').innerHTML = '50 - 25 = ' + subtraction;
+}
+
+function multiply() {
+    var multi = 5 * 5;
+    document.getElementById('Multi').innerHTML = '5 * 5 = ' + multi;
+}
+
+function divide() {
+    var divsion = 25 / 5;
+    document.getElementById('Division').innerHTML = ' 25 / 5 = ' + divsion;
+}
+
+function equation() {
+    var expression = (5 + 5) * 5 / 2 - 5;
+    document.getElementById('Maths').innerHTML = '5 plus 5 then multiplied by 5, divided by 2 then minus another 5 equals ' + expression;
+}
